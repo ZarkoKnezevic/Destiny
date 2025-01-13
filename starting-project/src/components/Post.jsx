34cf2e9ff058df4dcd519382566c31723ad991e0
knezevicz <zarkoknezevic@lx-media.at>
2025-01-13 15:54:32 +0100

    added Post css module

--- starting-project/src/components/Post.jsx
+++ starting-project/src/components/Post.jsx
@@ -1,12 +1,13 @@
+import classes from './Post.module.css';
 const names = ['Zarko', 'Ivan', 'Petar', 'Jovan', 'Marko'];
 
 function Post(props) {
     const chosenRandomName = names[Math.floor(Math.random() * names.length)];
 
     return (
-        <div>
-            <p>{ props.author }</p>
-            <p>{ props.body }</p>
+        <div className={classes.post}>
+            <p className={classes.author}>{ props.author }</p>
+            <p className={classes.text}>{ props.body }</p>
         </div>
     );
 }
